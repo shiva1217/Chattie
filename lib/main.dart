eb64f575eb4d65680377273cd5f1be027deca730
Shivangi singh <107601133+shiva1217@users.noreply.github.com>
2024-06-29 01:06:53 +0530

    7th

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/helper/helper_function.dart';
+import 'package:flutter_application_1/pages/auth/login_page.dart';
+import 'package:flutter_application_1/pages/home-page.dart';
+import 'package:flutter_application_1/shared/constants.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: Constants.apiKey,
+            appId: Constants.appId,
+            messagingSenderId: Constants.messagingSenderId,
+            projectId: Constants.projectId));
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isSignedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserLoggedInStatus();
+  }
+
+  getUserLoggedInStatus() async {
+    await HelperFunctions.getUserLoggedInStatus().then((value) {
+      if (value != null) {
+        setState(() {
+          _isSignedIn = value;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          primaryColor: Constants().primaryColor,
+          secondaryHeaderColor: Constants().secondaryColor,
+          scaffoldBackgroundColor: Colors.white),
+      debugShowCheckedModeBanner: false,
+      home: _isSignedIn ? const HomePage() : const LoginPage(),
+    );
+  }
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:418960215776:web:bc0386e20cfa24c3298a85";
+  static String apiKey = "AIzaSyACRdTFeS2z8BE1gwbiVzUWeZyVvMFPz1A";
+  static String messagingSenderId = "418960215776";
+  static String projectId = "chatty-38513";
+  final primaryColor = const Color(0xFF0160ed);
+  final secondaryColor = const Color(0xFF242424);
+}
